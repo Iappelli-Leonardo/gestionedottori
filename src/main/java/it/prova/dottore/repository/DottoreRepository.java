08af2cf3242078fba456c9c7fe356524ec8eb087
leoIapp <leoiappelli01@gmail.com>
2021-11-23 14:32:34 +0100

    impl dottorerepository

---
+++ src/main/java/it/prova/dottore/repository/DottoreRepository.java
@@ -0,0 +1,12 @@
+package it.prova.dottore.repository;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import it.prova.dottore.model.Dottore;
+
+public interface DottoreRepository extends PagingAndSortingRepository<Dottore, Long>, JpaSpecificationExecutor<Dottore> {
+
+	public Dottore findByCodiceDipendente(String codiceInput);
+	
+}
---
+++ src/main/java/it/prova/dottore/service/DottoreServiceImpl.java
@@ -0,0 +1,108 @@
+package it.prova.dottore.service;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.Predicate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import it.prova.dottore.exception.DottoreNotFoundException;
+import it.prova.dottore.model.Dottore;
+import it.prova.dottore.repository.DottoreRepository;
+import it.prova.dottore.service.DoottoreService.DottoreService;
+import org.springframework.data.domain.Sort;
+@Service
+public class DottoreServiceImpl implements DottoreService{
+
+	@Autowired
+	private DottoreRepository dottoreRepository;
+
+	@Override
+	public List<Dottore> listAll() {
+		return (List<Dottore>) dottoreRepository.findAll();
+	}
+
+	@Override
+	public Dottore cariscaSingoloElemento(Long id) {
+		return dottoreRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Dottore inserisciNuovo(Dottore transientInstance) {
+		return dottoreRepository.save(transientInstance);
+	}
+
+	@Override
+	public Dottore get(Long idInput) {
+		return dottoreRepository.findById(idInput)
+				.orElseThrow(() -> new DottoreNotFoundException("Element with id " + idInput + " not found."));
+	}
+
+	@Override
+	public Dottore save(Dottore input) {
+		return dottoreRepository.save(input);
+	}
+
+	@Override
+	public void delete(Dottore input) {
+		dottoreRepository.delete(input);
+	}
+
+	@Override
+	public Page<Dottore> searchAndPaginate(Dottore dottoreExample, Integer pageNo, Integer pageSize, String sortBy) {
+
+		Specification<Dottore> specificationCriteria = (root, query, cb) -> {
+
+			List<Predicate> predicates = new ArrayList<Predicate>();
+
+			if (!StringUtils.isEmpty(dottoreExample.getNome()))
+				predicates.add(cb.like(cb.upper(root.get("nome")), "%" + dottoreExample.getNome().toUpperCase() + "%"));
+
+			if (!StringUtils.isEmpty(dottoreExample.getCognome()))
+				predicates.add(
+						cb.like(cb.upper(root.get("cognome")), "%" + dottoreExample.getCognome().toUpperCase() + "%"));
+
+			if (!StringUtils.isEmpty(dottoreExample.getCodiceDipendente()))
+				predicates.add(cb.like(cb.upper(root.get("codiceDipendente")),
+						"%" + dottoreExample.getCodiceDipendente().toUpperCase() + "%"));
+			if (dottoreExample.isInServizio())
+				predicates.add(cb.isTrue(root.get("inServizio")));
+			else
+				predicates.add(cb.isFalse(root.get("inServizio")));
+
+			if (dottoreExample.isInVisita())
+				predicates.add(cb.isTrue(root.get("inVisita")));
+			else
+				predicates.add(cb.isFalse(root.get("inVisita")));
+
+			return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		};
+
+		Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+
+		return dottoreRepository.findAll(specificationCriteria, paging);
+	}
+
+	@Override
+	public Dottore findByCodiceDipendente(String codiceInput) {
+
+		return dottoreRepository.findByCodiceDipendente(codiceInput);
+	}
+
+	@Override
+	public Dottore impostaInVisita(String codiceDipendente) {
+		Dottore dottore = dottoreRepository.findByCodiceDipendente(codiceDipendente);
+		dottore.setInVisita(true);
+		return dottoreRepository.save(dottore);
+
+	}
+	
+}
